8aa1d5ffc844bf7a445e3663b3dd48dd8bf1d639
KyeGitHub <Kyehammond97@gmail.com>
2019-11-13 22:09:13 +0000

    Inital Commit

---
+++ Assets/InfoGamerPhoton/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PhotonView PV;
+    private CharacterController myCC;
+
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        myCC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PV.IsMine)
+        {
+            BasicMovement();
+            BasicRotation();
+        }
+    }
+
+    void BasicMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            myCC.Move(transform.forward * Time.deltaTime * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            myCC.Move(-transform.forward * Time.deltaTime * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            myCC.Move(transform.right * Time.deltaTime * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            myCC.Move(-transform.right * Time.deltaTime * moveSpeed);
+        }
+    }
+
+    void BasicRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+        transform.Rotate(new Vector3(0, mouseX, 0));
+    }
+}
